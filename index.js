62d94f5266e8a442d42e295901d357ec34c41f6e
ornellas <129.ornellas@gmail.com>
2025-04-17 22:48:00 -0300

    feat: inclui nodemon e metodo pra validar CEP

--- index.js
+++ index.js
@@ -3,9 +3,18 @@ const { Cep } = require('./services/consulta-cep');
 const app = express()
 const port = 3000
 
-app.get('/', (req, res) => {
-    endereco = new Cep
-    res.send(endereco.retornaEndereco('asdasdsa'));
+app.get('/', async (req, res) => {
+    const cep = new Cep()
+
+    if(cep.validaCep('241107305')){
+        try {
+            const endereco = await cep.retornaEndereco('24110705'); // CEP real pra testar
+            res.send(endereco); // manda o JSON como resposta
+        } catch (err) {
+            res.status(500).send('Erro ao buscar CEP');
+        }
+    }
+    return res.status(500).send('Cep digitado incorretamente, insira um Cep válido!');
 })
 
 app.listen(port, () => {
--- services/consulta-cep.js
+++ services/consulta-cep.js
@@ -3,8 +3,21 @@ class Cep {
     constructor(cep){
         this.cep = cep
     }
+
+    validaCep(cep) {
+        return /^[0-9]{8}$/.test(cep);
+    }
+
     retornaEndereco(cep){
-        return "endereço" + cep
+        return fetch(`http://viacep.com.br/ws/${cep}/json/`)
+            .then(response => response.json())
+            .then(data => {
+                return data;
+            })
+            .catch(err => {
+                console.error('Erro ao buscar CEP:', err);
+                throw err;
+            });
     }
     
 }
